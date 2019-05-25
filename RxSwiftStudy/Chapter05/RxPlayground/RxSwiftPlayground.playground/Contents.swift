0b7ce20efffba121f4fba3fef1245955fd9db798
Shawn Chun <shockw@hotmail.com>
2019-05-25 21:47:21 +0900

    :books: RxSwift Chapter05 studying

--- RxSwiftStudy/Chapter05/RxPlayground/RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftStudy/Chapter05/RxPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -32,3 +32,63 @@ import RxSwift
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
  */
+example(of: "ignoreElements") {
+	let strikes = PublishSubject<String>()
+	
+	let disposeBag = DisposeBag()
+	
+	strikes
+		.ignoreElements()
+		.subscribe { _ in
+			print("You're out!")
+		}.disposed(by: disposeBag)
+	
+	strikes.onNext("X")
+	strikes.onNext("X")
+	strikes.onNext("X")
+	
+	strikes.onCompleted()
+}
+
+example(of: "elementAt") {
+	let strikes = PublishSubject<String>()
+	let disposeBag = DisposeBag()
+	
+	strikes.elementAt(2)
+		.subscribe(onNext: { _ in
+			print("You're out!")
+		}).disposed(by: disposeBag)
+	
+	strikes.onNext("X")
+	strikes.onNext("X")
+	strikes.onNext("X")
+}
+
+example(of: "filter") {
+	let disposeBag = DisposeBag()
+	
+	Observable.of(1, 2, 3, 4, 5, 6)
+		.filter({
+			$0 % 2 == 0
+		}).subscribe(onNext: {
+			print($0)
+		}).disposed(by: disposeBag)
+}
+
+example(of: "skip") {
+	let disposeBag = DisposeBag()
+	Observable.of("A", "B", "C", "D", "E", "F")
+		.skip(3)
+		.subscribe(onNext: {
+			print($0)
+		}).disposed(by: disposeBag)
+}
+
+example(of: "skipWhile") {
+	let disposeBag = DisposeBag()
+	Observable.of(2, 2, 3, 4, 4).skipWhile { integer in
+		integer % 2 == 0
+		}.subscribe(onNext: {
+			print($0)
+		}).disposed(by: disposeBag)
+}
