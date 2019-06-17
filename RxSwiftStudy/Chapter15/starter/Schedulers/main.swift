59571c8e87bc4968c83c24fe56a196ac411d255a
Shawn Chun <shockw@hotmail.com>
2019-06-17 21:44:36 +0900

    :books: RxSwift chapter15 studying.

--- RxSwiftStudy/Chapter15/starter/Schedulers/main.swift
+++ RxSwiftStudy/Chapter15/starter/Schedulers/main.swift
@@ -36,28 +36,28 @@ let bag = DisposeBag()
 let animal = BehaviorSubject(value: "[dog]")
 
 animal
-  .subscribeOn(MainScheduler.instance)
-  .dump()
-  .observeOn(globalScheduler)
-  .dumpingSubscription()
-  .disposed(by: bag)
+	.subscribeOn(MainScheduler())
+	.dump()
+	//  .observeOn(globalScheduler)
+	.dumpingSubscription()
+	.disposed(by: bag)
 
 let fruit = Observable<String>.create { observer in
-  observer.onNext("[apple]")
-  sleep(2)
-  observer.onNext("[pineapple]")
-  sleep(2)
-  observer.onNext("[strawberry]")
-  return Disposables.create()
+	observer.onNext("[apple]")
+	sleep(2)
+	observer.onNext("[pineapple]")
+	sleep(2)
+	observer.onNext("[strawberry]")
+	return Disposables.create()
 }
 
 fruit
-  .subscribeOn(globalScheduler)
-  .dump()
-  .observeOn(MainScheduler.instance)
-  .dumpingSubscription()
-  .disposed(by: bag)
-
+	.subscribeOn(globalScheduler)
+	.dump()
+	.observeOn(MainScheduler())
+	.dumpingSubscription()
+	.disposed(by: bag)
+/**
 let animalsThread = Thread() {
   sleep(3)
   animal.onNext("[cat]")
@@ -71,5 +71,6 @@ let animalsThread = Thread() {
 
 animalsThread.name = "Animals Thread"
 animalsThread.start()
+*/
 
 RunLoop.main.run(until: Date(timeIntervalSinceNow: 13))
